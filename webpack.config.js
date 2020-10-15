6a2ee12f94e74cf6b2c5ea1a8edeb44466525e0f
HilmaM <hazelman@live.com>
2020-10-15 18:13:16 +0200

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-/*var HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+/*WebPack Config*/
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -77,62 +77,6 @@ module.exports = {
     // global app config object
     config: JSON.stringify({
       apiUrl: 'https://mudfam-server.herokuapp.com/'
-    })
-  }
-}*/
-
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-
-module.exports = {
-  mode: 'development',
-  entry: './src/index.jsx',
-  output:{
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.less$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          { loader: 'less-loader' }
-        ]
-      },
-      {
-        test: /\.svg$/,
-        loader: 'svg-inline-loader?classPrefix'
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ],
-      },
-    ]
-  },
-  resolve: {
-    mainFiles: ['index', 'Index'],
-    extensions: ['.js', '.jsx'],
-    alias: {
-      '@': path.resolve(__dirname, 'src/'),
-    }
-  },
-  plugins: [new HtmlWebpackPlugin({
-    template: './src/index.html'
-  })],
-  devServer: {
-    historyApiFallback: true
-  },
-  externals: {
-    // global app config object
-    config: JSON.stringify({
-      apiUrl: 'https://mudfam-server.herokuapp.com'
     })
   }
 }
